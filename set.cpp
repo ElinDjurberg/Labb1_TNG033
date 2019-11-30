f7483ca8d6430c4abce22c36cb187ed624482091
ElinDjurberg <57451642+ElinDjurberg@users.noreply.github.com>
2019-11-30 11:11:47 +0100

    Add files via upload

--- set.cpp
+++ set.cpp
@@ -1,24 +1,18 @@
 #include "set.hpp"
 
-
-
+// skapar ny nod i ett set
 void Set::insert(Node* p, int value) const {
-	//Kolla alla fallen, sist, mellan efter, dummynode före
+
 	auto newNode = new Node(value, nullptr);
-	if (p->next == nullptr) {
-		p->next = newNode;
-	}
 
-	else {
-
-		newNode->next = p->next;
-		p->next = newNode;
-	}
-	//borde man deleta här??
+	// uppdaterar pekarna
+	//insertar efter p
+	newNode->next = p->next;
+	p->next = newNode;
 }
 
 void Set::remove(Node* p) {
-	
+	//deletar noden som p pekar på.
 	delete p;
 	p = nullptr;
 
@@ -30,16 +24,15 @@ Set::Set()
 {
 	//I set.hpp class Node finns en constructor som behöver en int samt en pointer för initalizering
 
-	head = new Node(0, nullptr); // Fö 7, aida skrev det ska vara såhär!!
+	head = new Node(0, nullptr); 
 	//Detta är en nod med en tom efterföljare
-	// kan inte deleta här för då finns inget att skicka vidare //delete head;
+	// kan inte deleta här för då finns inget att skicka vidare 
 }
 
 // Constructor for creating a set from an int
 Set::Set(int v) : Set()
-{
+{	
 	head->next = new Node(v, nullptr);
-	//deleta ej här heller
 }
 
 // Constructor creating a set
@@ -49,10 +42,12 @@ Set::Set(const int a[], int n) :Set()
 	//Vi måste peka på array för den är constant så vi kan ej ändra den
 	int *newArray = new int[n];
 
+	//Använder pekaren för att lägga in värden från a i den nya arrayn vi har skapat. Nu kan vi komma åt dem
 	for (int i = 0; i < n; i++) {
 		newArray[i] = a[i];
 	}
 
+	//sortering
 	int temp = 0;
 	for (int i = 0; i < n - 1; i++) {
 		for (int j = i + 1; j < n; j++) {
@@ -69,68 +64,68 @@ Set::Set(const int a[], int n) :Set()
 
 	Node* dummy = head;
 
+	// tar värdena från den nya arrayen och sätter som values i ett nytt sett av noder ( singled linked list)
 	for (int i = 0; i < n; i++) {
 		int value = newArray[i];
-		//insert(dummy, i);
-		
-		Node* newNode = new Node(value, nullptr);//Repetera ej den koden
-		dummy->next = newNode; //sätter ihop noderna
+
+		insert(dummy, value);
+
+		// uppdatera dummy, annars hamnar noderna som bakvänt, man lägger in åt fel håll
 		dummy = dummy->next;
 	}
-	//Glöm inte att vi måste deleta på fler ställen!!
+	// om vi deletar newArray med hake [] tar vi bort hela arrayen som pekren newArray pekar på
 	delete[] newArray;
 	newArray = nullptr;
 
+	/*
+	Köra delete manuellt för remove funktionen tar bara bort noder
+	*/
 }
 
 
 
+//Kopiera från S och lägga in i R
 // copy constructor
 Set::Set(const Set& source) : Set()
 {
-
 	Node* temp = source.head; // head till S
-	temp = temp->next; // pekar på första riktiga noden
+	temp = temp->next; // pekar på första riktiga noden, hoppar över dummyNode
+
 	Node* tail = head; // denna ska följa med längs hela R, head till R
 
 	while (temp != nullptr) {
 
-		Node* newNode = new Node(temp->value, nullptr);
-		tail->next = newNode; // knyter ihop R
-		tail = tail->next; // samma som // tail=newNode;
-		temp = temp->next; // pekarp å nästa nod i S, som ska kopieras
+		/*Node* newNode = new Node(temp->value, nullptr);*/
+		/*tail->next = newNode; // knyter ihop R*/
+
+		insert(tail, temp->value);
+
+		tail = tail->next; // samma som tail=newNode;
+		temp = temp->next; // pekar på nästa nod i S, som ska kopieras
 	}
 }
-
-
-
 
 // Destructor: deallocate all nodes
 Set::~Set()
 {
 	//gå node för node och delete p;
-	//Obs glöm ej radera dummy nodes också?
-
-	Node* current = head;
 	// är redan inne i set, så vi når head
-
+	Node* current = head;
+	
 	//delete head;
 	while (current != nullptr) {
 
-		//går längst bak i listan
+		//börjar framifrån och går igenom hela listan
 		//finns dummy next så hoppa fram ett steg tills man kommit till slutet. 
 
 		Node* soon = current->next;
 
-		// delete *current; // för att ta bort noden?
-		// sådan man måste deleta själv är sådant man gjort New på, t.ex noder
+		// delete current;
+		remove(current);
 
-		delete current;//tar vi bort objectet
+		// sätter om current från nullptr i remove funktionen till soon
 		current = soon;
-
 	}
-
-
 }
 
 
@@ -150,7 +145,7 @@ kontrollera nmanet och ålder, INTE ändra något, endast kontrollera
 bool Set::empty() const
 {
 	return head->next == nullptr;
-	// returnerar true eller falsetrue om
+	// returnerar true eller false om
 	// true om head -> next==nullptr;
 	//false om head -> next!=nullptr;
 }
@@ -161,16 +156,16 @@ int Set::cardinality() const
 {
 	//whileloop och räkna antal
 	int counter = 0;
+	//ta head->next för att inte räkna med dummyNode
 	Node* dummy = head->next;
 
-	/* man kan även räkna hela skiten inklusive head, sedan ta counter-1 innan return */
 
 	while (dummy != nullptr) {
 		counter++;
 		dummy = dummy->next;
 	}
 
-	return counter; // to be deleted
+	return counter;
 }
 
 // Test if x is an element of the set
@@ -179,45 +174,46 @@ bool Set::member(int x) const
 	Node* search = head->next;
 
 	while (search != nullptr) {
-
+		//om man hittar värdet man söker efter return true och avsluta
 		if (search->value == x) {
 			return true;
 		}
 		search = search->next;
 	}
 
-	return false; //to be deleted
+	return false; 
 }
 
 // Assignment operator
 Set& Set::operator=(Set s)
-{
-
-	Set copy(s);
-	std::swap(head, copy.head);
-
-	return *this; // hela objektet returneras
-	// to be deleted raderas vår automatisk? Kallar den på delocate automatiskt?. Står ju att vi ska radera
+{  //s.head no copy
+	//copy and swap idiom
+	//Set copy(s);
+	std::swap(head, s.head);
+	// hela objektet returneras
+	//delocate automatiskt
+	return *this; 
+	
 }
 
+//Kollar om det är ett subset/delmmängd
 bool Set::operator<=(const Set& b) const
 {
 	bool isSubset = false;
 	Node* dummy = head->next;//hoppar över till första tal
 	Node* dummyB = b.head->next;
 	int counter = 0;
-	
 
 	//Kollar om vi är i en tom för då kommer det alltid vara sant
 	if (this->empty()) {
 		isSubset = true;
 		return isSubset;
-		
 	}
 
 	while (dummy != nullptr) {
 
 		while (dummyB != nullptr) {
+
 			if (dummyB->value == dummy->value) {
 				counter++;
 			}
@@ -229,152 +225,44 @@ bool Set::operator<=(const Set& b) const
 		dummy = dummy->next;
 
 	}
-	
+	//om den har hittat lika många element som finns i listan som den är lång så är det en delmängd
+	//alla talen i det lilla settet ska finnas i det större. Countern ökar varje gång det lilla settet->value är samma som stora settets value
 	if (counter == this->cardinality()) {
 		isSubset = true;
 	}
-	
-	return isSubset; 
+
+	return isSubset;
 }
 
 bool Set::operator==(const Set& b) const
 {
 	
-	bool isSubset = false;
-	Node* dummy = head->next;//hoppar över till första tal
-	Node* dummyB = b.head->next;
-	int counter = 0;
-
-
-	while (dummy != nullptr) {
-
-		while (dummyB != nullptr) {
-			if (dummyB->value == dummy->value) {
-				counter++;
-			}
-			dummyB = dummyB->next;
-
-		}
-		//resetta dummy 
-		dummyB = b.head->next;
-		dummy = dummy->next;
-
-	}
-	
-	
-	/*
-	bool isSubset = false;
-	bool hej = false;
-	bool da = false;
-
-	if (b.cardinality() == this->cardinality()) {
-		hej = true;//blir sant
-	}
-	if ((this <= &b)) {
-		da = true;//blir falsk
-	}
-	*/
-	/*
-	//(this <= &b)(&b <= this)//får kan väl ej skicka in this som argument
-	if ((b.cardinality() == this->cardinality()) && (this <= &b)) {
-		//This kan kanske inte skickas som det?
-		isSubset = true;
-
-	}*/
-	
-	
-	//kollar att båda setten innehåller lika mycket samt att allt i enna settet även finns i andra
-	if ((counter == this->cardinality()) && b.cardinality() == this->cardinality()) {
-		isSubset = true;
-	}
-	
-	return isSubset;  // to be deleted
-
+	return !(*this != b);
 }
 
 bool Set::operator!=(const Set& b) const
 {
-	//if(this->operator==() != b.operator==())
+	
+	bool isSubset = true;
 
-	bool isSubset = false;
-	bool answer = true;
-	Node* dummy = head->next;//hoppar över till första tal
-	Node* dummyB = b.head->next;
-	int counter = 0;
-	//bool da = b.empty();//b borde vara tom?//tom är det som skickas in
-	//bool hej =this->empty();
-
-	while (dummy != nullptr) {
-
-		while (dummyB != nullptr) {
-			if (dummyB->value == dummy->value) {
-				counter++;
-			}
-			dummyB = dummyB->next;
-
-		}
-		//resetta dummy 
-		dummyB = b.head->next;
-		dummy = dummy->next;
-
+	if ((*this <= b) && (b.cardinality() == this->cardinality())) {
+		isSubset = false;
 	}
-	//kollar att båda setten innehåller lika mycket samt att allt i enna settet även finns i andra
-	if ((counter == this->cardinality()) && b.cardinality() == this->cardinality()) {
-		isSubset = true;
-	}
-
-	if(isSubset == true){
-		answer = false;
-	}
-	//return answer;  // to be deleted
-	return !(isSubset);
+	
+	return isSubset;
 
 }
 
 bool Set::operator<(const Set& b) const
 {
-/*
-	bool isSubset = false;
-	Node* dummy = head->next;//hoppar över till första tal
-	Node* dummyB = b.head->next;
-	int counter = 0;
 
-
-	while (dummy != nullptr) {
-
-		while (dummyB != nullptr) {
-			if (dummyB->value == dummy->value) {
-				counter++;
-			}
-			dummyB = dummyB->next;
-
-		}
-		//resetta dummy 
-		dummyB = b.head->next;
-		dummy = dummy->next;
-
-	}
-	//int y = b.cardinality();
-	//kan man kalla på cardinality sådär?
-	//Vi har ju skrivit om > nu kan jag fortfarande använda den som förut?
-	//den vi skickar in måste vara större
-
-	*/
 	bool isSubset = false;
 
 	if ((this <= &b) && (b.cardinality() > this->cardinality())) {
 		isSubset = true;
-
 	}
 
-
-	/*
-	if ((counter == this->cardinality()) && b.cardinality() > this->cardinality()) {
-		isSubset = true;
-	}
-	*/
-	return isSubset;  // to be deleted
-	
+	return isSubset;  
 }
 
 // Set union
@@ -382,45 +270,45 @@ bool Set::operator<(const Set& b) const
 Set Set::operator+(const Set& b) const
 {
 	// Add code
-	//Set copy(s);
-	//std::swap(head, copy.head);
-	Set newSet = Set();//Dummy node till vår nya sätt
+	Set newSet = Set();//Dummy node till vårt nya set
 
-	Node* dummyB = b.head->next;
-	Node* dummyR = head->next;	//Når vi setet vi är i sett R//Ja vi når R
+	Node* pointerB = b.head->next;
+	Node* pointerR = head->next;	//Når vi setet vi är i sett R//Ja vi når R
 	//vi skriver head next för att inte räkna med vår dummynode!
 
-	Node* dummyNew = newSet.head;//???? ska vi ha kvar den eller byta till dummy new direkt?
+	Node* dummyNew = newSet.head;
 
 
-	//Node* headNew =   //vad ska dessa vara??
-	//Node* dummyB = new Node(0, nullptr);
+	while ((pointerB != nullptr) && (pointerR != nullptr)) {
 
-
-	while ((dummyB != nullptr) && (dummyR != nullptr)) {
-
-		if (dummyB->value < dummyR->value) {
+		if (pointerB->value < pointerR->value) {
 			//skapa en ny lista
-			dummyNew->next = new Node(dummyB->value, nullptr);
-			//de* List = new Node()
 
-			dummyB = dummyB->next;
+			insert(dummyNew, pointerB->value);
+			//dummyNew->next = new Node(pointerB->value, nullptr);
+			pointerB = pointerB->next;
 			dummyNew = dummyNew->next;
 		}
 
-		else if (dummyR->value < dummyB->value) {
+		else if (pointerR->value < pointerB->value) {
 			//lägga till på lista
-			dummyNew->next = new Node(dummyR->value, nullptr);
-			dummyR = dummyR->next;
+			insert(dummyNew, pointerR->value);
+			//dummyNew->next = new Node(pointerR->value, nullptr);
+			pointerR = pointerR->next;
 			dummyNew = dummyNew->next;
 		}
 
 		else {
 			//om de är samma 
 			//lägg till i list
-			dummyNew->next = new Node(dummyR->value, nullptr);
-			dummyR = dummyR->next;
-			dummyB = dummyB->next;
+
+		/*Node* newNode = new Node(temp->value, nullptr);*/
+		/*tail->next = newNode; // knyter ihop R*/
+			insert(dummyNew, pointerR->value);
+
+			//dummyNew->next = new Node(dummyR->value, nullptr);
+			pointerR = pointerR->next;
+			pointerB = pointerB->next;
 			dummyNew = dummyNew->next;
 		}
 
@@ -428,18 +316,26 @@ Set Set::operator+(const Set& b) const
 	}
 
 	//Lägg till i listorna
-	while (dummyB != nullptr) {
-		//Lägg till i listan
-		dummyNew->next = new Node(dummyB->value, nullptr);
-		dummyB = dummyB->next;
+	while (pointerB != nullptr) {
+		//Lägg till resterande i listan
+
+
+		//dummyNew->next = insert
+		insert(dummyNew, pointerB->value);
+		//dummyNew->next = new Node(dummyB->value, nullptr);
+		pointerB = pointerB->next;
 		dummyNew = dummyNew->next;
 
 	}
 
-	while (dummyR != nullptr) {
-		//Lägg till i listan
-		dummyNew->next = new Node(dummyR->value, nullptr);
-		dummyR = dummyR->next;
+	while (pointerR != nullptr) {
+		//Lägg till resten i listan
+
+
+
+		insert(dummyNew, pointerR->value);
+		//dummyNew->next = new Node(pointerR->value, nullptr);
+		pointerR = pointerR->next;
 		dummyNew = dummyNew->next;
 
 	}
@@ -450,20 +346,15 @@ Set Set::operator+(const Set& b) const
 // Set intersection
 Set Set::operator*(const Set& b) const
 {
-	// Add code
-
-
+	
 	Set newSet = Set();//Dummy node till vår nya sätt
 
 	Node* dummyB = b.head->next;
 	Node* dummyR = head->next;	//Når vi setet vi är i sett R//Ja vi når R
 	//vi skriver head next för att inte räkna med vår dummynode!
 
-	Node* dummyNew = newSet.head;//???? ska vi ha kvar den eller byta till dummy new direkt?
+	Node* dummyNew = newSet.head;
 
-
-	//Node* headNew =   //vad ska dessa vara??
-	//Node* dummyB = new Node(0, nullptr);
 
 	while (dummyB != nullptr) {
 
@@ -471,7 +362,9 @@ Set Set::operator*(const Set& b) const
 
 			if (dummyB->value == dummyR->value) {
 				//skapa en ny lista
-				dummyNew->next = new Node(dummyB->value, nullptr);
+
+				insert(dummyNew, dummyB->value);
+				//dummyNew->next = new Node(dummyB->value, nullptr);
 				//de* List = new Node()
 
 				dummyNew = dummyNew->next;
@@ -516,7 +409,8 @@ Set Set::operator-(const Set& b) const
 		dummyB = b.head->next;//vi måste resetta dummy B till nästa loop
 
 		if (counter == 0) {
-			dummyNew->next = new Node(dummyR->value, nullptr);
+			insert(dummyNew, dummyR->value);
+			//dummyNew->next = new Node(dummyR->value, nullptr);
 			dummyNew = dummyNew->next;
 		}
 
@@ -529,42 +423,6 @@ Set Set::operator-(const Set& b) const
 // Set union with set {x}
 Set Set::operator+(int x) const
 {
-	//är en konstant så vi kan ej ändra den. Så vi kopierar den. Finns det ett bättre sätt?
-	//Node* dummyR = newSet.head;//hoppar över dummyNoden??Den har ingen dummyNode?
-	//int *newArray = new int[n];
-	//varför funkar detta inte????????????????
-
-	//Set newSet = Set();
-
-	//Node* dummyR = head->next;//i den vi är i
-	//Node* dummyNew = newSet.head;
-	//if (dummyR != nullptr) {
-	//	dummyNew->next = new Node(dummyR->value, nullptr);
-	//	dummyR = dummyR->next;
-	//	dummyNew = dummyNew->next;
-	//}
-
-
-	////dummyNew = newSet.head->next;//hoppa över dummy node. Nollställer
-	//dummyR = head->next;
-	//int counter = 0;
-
-	//while (dummyR != nullptr) {
-
-	//	if (dummyR->value == x) {
-
-	//		counter++;
-	//	}
-	//	dummyR = dummyR->next;
-	//}
-
-	//if (counter == 0) {
-	//	dummyNew->next = new Node(x, nullptr);
-	//}
-
-	////dummyNew = dummyNew->next;
-
-	//return newSet;  // to be deleted
 
 	//gör ett nytt sätt med det värdet vi fick in
 	Set *newInt = new Set(x);
@@ -574,12 +432,6 @@ Set Set::operator+(int x) const
 	delete newInt;//vi behöver deleta annars memory leaks
 	return combinedSet;
 
-	//Set *theOne = new Set(x);
-
-	//const Set s3 = *this + *theOne;
-
-	//// Add code
-	//return s3;  // to be deleted
 }
 
 // Set difference with set {x}
--- set.hpp
+++ set.hpp
@@ -5,71 +5,71 @@
 
 class Set {
 public:
-    // Default constructor
-    Set();
+	// Default constructor
+	Set();
 
-    // Constructor for creating a set from an int
-    explicit Set(int v);
+	// Constructor for creating a set from an int
+	explicit Set(int v);
 
-    // Constructor creating a set
-    // from n integers in a non-sorted array a
-    Set(const int a[], int n);
+	// Constructor creating a set
+	// from n integers in a non-sorted array a
+	Set(const int a[], int n);
 
-    // copy constructor
-    Set(const Set& b);
+	// copy constructor
+	Set(const Set& b);
 
-    // assignment operator
-    Set& operator=(Set b);
+	// assignment operator
+	Set& operator=(Set b);
 
-    // destructor
-    ~Set();
+	// destructor
+	~Set();
 
-    bool empty() const;        // Test if set is empty
-    int cardinality() const;   // Return number of elements in the set
-    bool member(int x) const;  // Test if x is an element of the set
+	bool empty() const;        // Test if set is empty
+	int cardinality() const;   // Return number of elements in the set
+	bool member(int x) const;  // Test if x is an element of the set
 
-    // Overloaded operators
-    // A new set is returned
-    Set operator+(const Set& b) const;  // Set union
-    Set operator*(const Set& b) const;  // Set intersection
-    Set operator-(const Set& b) const;  // Set difference
-    Set operator+(int x) const;         // set union with set {x}
-    Set operator-(int x) const;         // set difference with set {x}
+	// Overloaded operators
+	// A new set is returned
+	Set operator+(const Set& b) const;  // Set union
+	Set operator*(const Set& b) const;  // Set intersection
+	Set operator-(const Set& b) const;  // Set difference
+	Set operator+(int x) const;         // set union with set {x}
+	Set operator-(int x) const;         // set difference with set {x}
 
-    // Overloaded comparison operators
-    bool operator<=(const Set& b) const;
-    bool operator==(const Set& b) const;
-    bool operator!=(const Set& b) const;
-    bool operator<(const Set& b) const;
+	// Overloaded comparison operators
+	bool operator<=(const Set& b) const;
+	bool operator==(const Set& b) const;
+	bool operator!=(const Set& b) const;
+	bool operator<(const Set& b) const;
 
 private:
-    // This class is private to class Set
-    // but all class Node members are public to class Set
-    class Node {
-    public:
-        // Constructor
-        Node(int nodeVal, Node* nextPtr) : value(nodeVal), next(nextPtr) {
-        }
+	// This class is private to class Set
+	// but all class Node members are public to class Set
+	class Node {
+	public:
+		// Constructor
+		Node(int nodeVal, Node* nextPtr) : value(nodeVal), next(nextPtr) {
+		}
 
-        int value;
-        Node* next;
+		int value;
+		Node* next;
 
-        friend std::ostream& operator<<(std::ostream& os, const Set& b);
-    };
+		friend std::ostream& operator<<(std::ostream& os, const Set& b);
+	};
 
-    Node* head;  // points to the first node
-                 // Note: first node is a dummy node of the list
+	Node* head;  // points to the first node
+				 // Note: first node is a dummy node of the list
 
-    friend std::ostream& operator<<(std::ostream& os, const Set& b);
+	friend std::ostream& operator<<(std::ostream& os, const Set& b);
 
-    /* Auxiliarly functions */
+	/* Auxiliarly functions */
 
-    // Insert a new node after node pointed by p
-    // the new node should store value
-    void insert(Node* p, int value) const;
+	// Insert a new node after node pointed by p
+	// the new node should store value
+	void insert(Node* p, int value) const;
 
-    // Remove the node pointed by p
-    void remove(Node* p);
+	// Remove the node pointed by p
+	void remove(Node* p);
 };
 
 #endif
